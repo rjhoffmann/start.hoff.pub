0242d030d4f328dc5daa8999c10df1e264d7187e
Ryan Hoffmann <ryan.hoffmann@airwis.com>
2018-12-14 15:31:32 -0600

    task: init list & search components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,21 @@ import { graphql } from 'gatsby';
 
 import Page from '../components/page';
 
+// class StartPage extends React.Component {
+//   render() {
+//     const { edges: photos } = this.props.data.allUnsplashPhoto;
+//     const { node: photo } = _sample(photos);
+
+//     return (
+//       <Page photo={photo}>
+//         <div>
+//           <span>Hello World!</span>
+//         </div>
+//       </Page>
+//     )
+//   }
+// }
+
 export default ({ data: { allUnsplashPhoto: { edges: photos } } }) => {
   const { node: photo } = _sample(photos);
   
